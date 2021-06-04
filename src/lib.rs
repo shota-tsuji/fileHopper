7fe9005b469235cfe46fe4e8967db44c518ef1af
shota-tsuji <shotatuzi@gmail.com>
2021-06-05 06:00:53 +0900

    feat: notify crateの動作確認用のテストを追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,36 @@
+use notify::DebouncedEvent::Create;
+use notify::{watcher, RecursiveMode, Watcher};
+use std::fs;
+use std::fs::File;
+use std::path::PathBuf;
+use std::sync::mpsc::channel;
+use std::time::Duration;
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    // 確認対象のディレクトリにファイルを作成するとCreateイベントと作成したファイル名が返ってくる
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn when_create_then_create_event() {
+        const TARGET_DIR: &'static str = "/tmp/test/notify/";
+
+        let taret_path = PathBuf::from(TARGET_DIR);
+        let expected_path = PathBuf::from(TARGET_DIR.to_string() + "file1");
+
+        let _ = fs::create_dir_all(taret_path.as_path());
+        let _ = fs::remove_file(expected_path.as_path());
+
+        let (tx, rx) = channel();
+        let mut watcher = watcher(tx, Duration::from_secs(1)).unwrap();
+
+        watcher
+            .watch(taret_path.as_path(), RecursiveMode::Recursive)
+            .unwrap();
+
+        let _ = File::create(expected_path.as_path());
+
+        let result = rx.recv().unwrap();
+        assert_eq!(Create(expected_path), result);
     }
 }
